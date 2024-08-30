f0ad1edb6517c656f058004793d7b8f1f61a89da
massivebird <gdrakemail@gmail.com>
2024-08-29 23:22:43 -0400

    feat: generate 0..=255 as hexadecimal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use rand::prelude::*;
+
 fn main() {
     let mut rng = rand::thread_rng();
-    println!("Hello, world!");
+
+    let number: u32 = rng.gen_range(0..=255);
+
+    let as_hex: String = num_to_hex(number);
+
+    println!("{number} -> {as_hex}");
+}
+
+fn num_to_hex(n: u32) -> String {
+    let digit_converter = |digit: u32| -> char {
+        match digit {
+            n @ 0..=9 => n.to_string().chars().next().unwrap(),
+            10 => 'A',
+            11 => 'B',
+            12 => 'C',
+            13 => 'D',
+            14 => 'E',
+            15 => 'F',
+            _ => panic!("dude"),
+        }
+    };
+
+    let sixteens = n / 16;
+    dbg!(sixteens);
+    let ones = n - (16 * sixteens);
+    dbg!(ones);
+    dbg!(digit_converter(sixteens));
+
+    format!("x{}{}", digit_converter(sixteens), digit_converter(ones))
 }
