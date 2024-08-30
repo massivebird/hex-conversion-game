c60a3d470331965fdd12bdaff372dc3b204d9df2
massivebird <gdrakemail@gmail.com>
2024-08-30 00:25:05 -0400

    added: comments

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,8 @@ fn num_to_hex(n: u32) -> String {
 
     let mut hex_chars: Vec<char> = Vec::new();
 
-    let digit_converter = |digit: u32| -> char {
+    // Converts 0..=15 to the corresponding hex character.
+    let single_digitizer = |digit: u32| -> char {
         match digit {
             // +48 maps range 0..=9 to ASCII range '0'..='9'
             n @ 0..=9 => char::from_u32(n + 48).unwrap(),
@@ -43,17 +44,17 @@ fn num_to_hex(n: u32) -> String {
         }
     };
 
-    // The actual number-to-hex computation
+    // The actual input-number-to-hex computation
     {
         // We will decrement a copy of n until it reaches zero
         let mut remainder = n;
 
         for power in (0..=largest_power_of_16).rev() {
-            // Compute next digit value.
+            // Compute next digit value 0..=15.
             // "How many times does 16^{power} fit into the remainder?"
             let digit_value = remainder / 16u32.pow(power);
 
-            hex_chars.push(digit_converter(digit_value));
+            hex_chars.push(single_digitizer(digit_value));
 
             // compute working remainder of n to prepare for next iteration
             remainder -= digit_value * 16u32.pow(power);
